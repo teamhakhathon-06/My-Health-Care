8a91f06b5d02142ed6ffac17fccf6bc807ac8511
teamhakhathon-06 <teamhakhathon@gmail.com>
2026-08-27 23:19:39 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    MEDVAULT PWA SERVICE WORKER
    ========================================================= */
 
-const CACHE_NAME = "medvault-shell-v5";
+const CACHE_NAME = "medvault-shell-v6";
 
 const APP_SHELL = [
   "./",
